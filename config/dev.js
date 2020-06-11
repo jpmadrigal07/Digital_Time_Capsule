a0cfc86128f73c69c453474b85a93b0e7ee55a39
JP Madrigal <contact@jpmadrigal.com>
2020-06-11 23:07:51 +0800

    Create dev.js

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+// DEV.JS - DEVELOPMENT KEYS
+module.exports = {
+    googleClientID: '160426820364-2vf6s37k8brftd0kmf31vneag2n4c5a9.apps.googleusercontent.com', 
+    googleClientSecret: 'Nq9bFDHIVkK78gm_IcMylGV6',
+    facebookClientID: '2494096144157734', 
+    facebookClientSecret: 'd1547b59950d8e4c6a41e8ae2edf8873',
+    mongoURI: 'mongodb://poyiii22:patrick22@ds111063.mlab.com:11063/ccp',
+    cookieKey: 'SASasd655w431234',
+    redirectDomain: 'http://localhost:3000/'
+};
